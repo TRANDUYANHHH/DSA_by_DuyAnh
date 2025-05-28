2dcd03f339d619941382764d1388d131094144b7
TRANDUYANHHH <162913482+TRANDUYANHHH@users.noreply.github.com>
2025-05-28 16:15:20 +0700

    Add files via upload

--- a.cpp
+++ a.cpp
@@ -17,23 +17,33 @@ int main(){
 	while(t--){
 		int n;
 		cin >> n;
-		vector<pair<int,int>> a(n);
+		vector<int> a(n);
 		for(int i = 0; i < n; i++){
-			cin >> a[i].first;
-			a[i].second = i;
+			cin >> a[i];
 		}
-		sort(all(a));
-		int l = 0;
-		int mx = -1;
-		int min_idx = n + 1;
-		for(int i = 0; i < n; i++){
-			while(a[l].first < a[i].first){
-				min_idx = min(min_idx, a[l].second);
-				l++;
+		vector<int> I(n), D(n);
+		I[0] = 1; D[n - 1] = 1;
+		for(int i = 1; i < n; i++){
+			if(a[i] > a[i - 1]){
+				I[i] = I[i - 1] + 1;
+			} else {
+				I[i] = 1;
 			}
-			mx = max(mx, a[i].second - min_idx);
 		}
-		cout << mx << endl;
+		for(int i = n - 2; i >= 0; i--){
+			if(a[i] > a[i + 1]){
+				D[i] = D[i + 1] + 1;
+			} else {
+				D[i] = 1;
+			}
+		}
+		int ans = max(I[n - 1], D[0]);
+		for(int i = 1; i < n - 1; i++){
+			if(a[i] > a[i - 1] && a[i] > a[i + 1]){
+				ans = max(ans, 1 + I[i - 1] + D[i + 1]);
+			}
+		}
+		cout << ans << endl;
 	}
 	return 0;
 }
