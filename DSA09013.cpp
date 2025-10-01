d6d0f5612c101192237d39ef939652a014f38c80
TRANDUYANHHH <162913482+TRANDUYANHHH@users.noreply.github.com>
2025-10-02 00:47:02 +0700

    Update DSA09013.cpp

--- DSA09013.cpp
+++ DSA09013.cpp
@@ -1,60 +1,74 @@
-/**
- *  author: @tranDuyAnh
- **/
-#include <bits/stdc++.h>
+// @author : tranDuyAnh
 
+#include <bits/stdc++.h>
 using namespace std;
 
 #define ll long long
 #define all(x) (x).begin(), (x).end()
-const int maxn = 1e3 + 5, mod = 1e9 + 7;
-int n, m;
-vector<int> G[maxn];
-vector<pair<int,int>> E;
-bool vis[maxn];
-void DFS(int u, pair<int, int> edge){
-	vis[u] = true;
-	for(int x : G[u]){
-		pair<int,int> t = {min(u, x), max(u, x)};
-		if(t != edge && !vis[x]){
-			DFS(x, edge);
-		}
-	}
+int M[1001][1001];
+vector<int> G[1001];
+bool vis[1001];
+void dfs(int u){
+    vis[u] = true;
+    for(int x : G[u]){
+        if(!vis[x] && M[u][x]){
+            dfs(x);
+        }
+    }
 }
-
 void solve(){
-	cin >> n >> m;
-	for(int i = 1; i <= n; i++) G[i].clear();
-	E.clear();
-	for(int i = 0; i < m; i++){
-		int x, y;
-		cin >> x >> y;
-		G[x].push_back(y);
-		G[y].push_back(x);
-		E.emplace_back(min(x, y), max(x, y));
-	}
-	sort(all(E));
-	for(pair<int,int> x : E){
-		memset(vis, false, sizeof(vis));
-		DFS(1, x);
-		for(int i = 1; i <= n; i++) {
-			if(!vis[i]){
-				cout << x.first << ' ' << x.second << ' ';
-				break;
-			}
-		}
-	}
+    memset(M, 0, sizeof(M));
+    memset(vis, 0, sizeof(vis));
+    int N, m;
+    cin >> N >> m;
+    for(int i = 0; i < N; i++) G[i].clear();
+    vector<pair<int,int>> E;
+    for(int i = 0; i < m; i++){
+        int u, v;
+        cin >> u >> v;
+        u--, v--;
+        if(u > v) swap(u, v);
+        E.emplace_back(u, v);
+        M[u][v] = 1;
+        M[v][u] = 1;
+        G[u].push_back(v);
+        G[v].push_back(u);
+    }
+    int C = 0;
+    for(int i = 0; i < N; i++){
+        if(!vis[i]){
+            C++;
+            dfs(i);
+        }
+    }
+    sort(all(E));
+    for(auto p : E){
+        M[p.first][p.second] = 0;
+        M[p.second][p.first] = 0;
+        memset(vis, 0, sizeof(vis));
+        int cnt = 0;
+        for(int i = 0; i < N; i++){
+            if(!vis[i]){
+                cnt++;
+                dfs(i);
+            }
+        }
+        if(cnt > C){
+            cout << p.first + 1 << ' ' << p.second + 1 << ' ';
+        }
+        M[p.first][p.second] = 1;
+        M[p.second][p.first] = 1;
+    }
 }
 
-int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	int t;
-	cin >> t;
-	while(t--){
-		solve();
-		cout << '\n';
-	}
-	return 0;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while(t--){
+        solve();  
+        cout << '\n';
+    }
+    return 0;
 }
-
